b333f4fb161bdae1ceaf95a26436daab92e24b1b
Willian <Willian@willian-rmbp.local>
2014-01-20 02:06:58 +0800

    选取文件位置

---
+++ ScaningScene.m
@@ -0,0 +1,13 @@
+//
+//  ScaningScene.m
+//  osu
+//
+//  Created by Willian on 14-1-20.
+//  Copyright (c) 2014年 Willian-Zhang. All rights reserved.
+//
+
+#import "ScaningScene.h"
+
+@implementation ScaningScene
+
+@end
--- osu/AppDelegate.m
+++ osu/AppDelegate.m
@@ -15,18 +15,37 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    /* Pick a size for the scene */
-    SKScene *scene = [MyScene sceneWithSize:CGSizeMake(1024, 768)];
-
-    /* Set the scale mode to scale to fit the window */
+    [self selectFolder];
+    
+}
+- (void)selectFolder{
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    [panel setCanChooseFiles:NO];
+    [panel setCanChooseDirectories:YES];
+    [panel setCanCreateDirectories:YES];
+    [panel setPrompt:@"OK"];
+    [panel setTitle:@"Please select the osu! song dirctory you store your osu beatmaps"];
+    panel.allowsMultipleSelection = NO;
+    
+    [panel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result){
+        if (result == NSFileHandlingPanelOKButton) {
+            NSURL *songsDir = panel.URL;
+            //NSLog(@"%@",[songsDir.path stringByResolvingSymlinksInPath]);
+            
+        }else if (result == NSFileHandlingPanelCancelButton){
+            [NSApp terminate:self];
+        }
+    }];
+}
+- (void)persentSelectorScene{
+    SKScene *scene = [MyScene sceneWithSize:CGSizeMake(1280, 720)];
     scene.scaleMode = SKSceneScaleModeAspectFit;
-
+    
     [self.skView presentScene:scene];
-
+    
     self.skView.showsFPS = YES;
     self.skView.showsNodeCount = YES;
 }
-
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
     return YES;
 }
