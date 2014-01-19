44e81e2af2c501a81c4cfb7c5f37213da9eb7734
Willian <Willian@willian-rmbp.local>
2014-01-20 04:43:24 +0800

    Main Scene Added

---
+++ osu/MainScene.h
@@ -0,0 +1,13 @@
+//
+//  MainScene.h
+//  osu
+//
+//  Created by Willian on 14-1-20.
+//  Copyright (c) 2014年 Willian-Zhang. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MainScene : SKScene
+
+@end
